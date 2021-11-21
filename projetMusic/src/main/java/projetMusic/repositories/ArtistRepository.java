e70a0b012c083fe6be8ede6e5b5ba4891cffc8a7
SebAll95 <sebastien.alleaume.1995@gmail.com>
2021-11-21 15:57:14 +0100

    maj mineure2

--- projetMusic/src/main/java/projetMusic/repositories/ArtistRepository.java
+++ projetMusic/src/main/java/projetMusic/repositories/ArtistRepository.java
@@ -1,9 +1,27 @@
 package projetMusic.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import projetMusic.entity.Artist;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
+	@Query("select art from Artist art left join fetch art.albums")
+	List<Artist> findAlll();
+
+	@Query("select art from Artist art left join fetch art.albums as alb left join fetch alb.musics as mus where art.name=:name")
+	List<Artist> findByName(@Param("name") String name);
+
+	@Query("select art from Artist art left join fetch art.albums where art.albums =:album")
+	List<Artist> findByAlbum(@Param("album") String album);
+
+	@Query("select art from Artist art left join fetch art.albums as alb left join fetch alb.musics as mus where alb.musics=:music")
+	List<Artist> findByMusic(@Param("music") String music);
+
+	@Query("select art from Artist art left join fetch art.albums as alb left join fetch alb.musics as mus where alb.musics=:genre")
+	List<Artist> findByGenre(@Param("genre") String genre);
 }
--- projetMusic/src/main/java/projetMusic/repositories/MusicRepository.java
+++ projetMusic/src/main/java/projetMusic/repositories/MusicRepository.java
@@ -1,9 +1,27 @@
 package projetMusic.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import projetMusic.entity.Music;
 
 public interface MusicRepository extends JpaRepository<Music, Long> {
 
+	@Query("select mus from Music mus left join fetch mus.albums as alb left join fetch alb.artists as art")
+	List<Music> findAll();
+
+	@Query("select mus from Music mus left join fetch mus.albums as alb left join fetch alb.artists as art where mus.title=:title")
+	List<Music> findByTitle(@Param("title") String title);
+
+	@Query("select mus from Music mus left join fetch mus.albums where mus.albums=:album")
+	List<Music> findByAlbum(@Param("album") String album);
+
+	@Query("select mus from Music mus left join fetch mus.playlists where mus.playlists=:playlist")
+	List<Music> findByPlaylist(@Param("playlist") String playlist);
+
+	@Query("select mus from Music mus left join fetch mus.albums as alb left join fetch alb.artists as art where art=:artist")
+	List<Music> findByArtist(@Param("artist") String artist);
 }
