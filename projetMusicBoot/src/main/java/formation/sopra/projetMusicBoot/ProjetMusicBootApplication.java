c21d8e9ea3e60b41de2ec1522dcd6ad121eb1e24
Théo Bourgeais <theo.bourgeais@gmail.com>
2021-12-01 09:17:31 +0100

    Creation projet Spring Boot

---
+++ projetMusicBoot/src/main/java/formation/sopra/projetMusicBoot/ProjetMusicBootApplication.java
@@ -0,0 +1,13 @@
+package formation.sopra.projetMusicBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMusicBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMusicBootApplication.class, args);
+	}
+
+}
