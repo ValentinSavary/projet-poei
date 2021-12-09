2f6f9006d803d9fea70b2a5f8bf3106ddc871bc2
Théo Bourgeais <theo.bourgeais@gmail.com>
2021-12-09 14:39:48 +0100

    Creation structure/arborescence
    services, components, model, ...

--- projetMusicAngular/src/app/app.module.ts
+++ projetMusicAngular/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumComponent } from './components/album/album.component';
+import { MusicComponent } from './components/music/music.component';
+import { UserComponent } from './components/user/user.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ArtistComponent,
+    AlbumComponent,
+    MusicComponent,
+    UserComponent,
+    PlaylistComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
